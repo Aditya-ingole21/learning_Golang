fa85998888c5e26d69e9a6bea6e27e8348b77f07
Aditya-ingole21 <aditayingole113@gmail.com>
2025-12-15 15:05:25 +0530

    learned shorthand variable declaration

---
+++ Basics/variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+// explicit variable declaration with type
+var name string = "Golang"
+// explicit variable declaration
+var age = 10
+// short hand variable declaration
+city := "nagpur"
+fmt.Println(name)
+fmt.Println(age)
+fmt.Println(city)
+}
